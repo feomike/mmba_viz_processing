44a32346679fbc9f078884b7aaea7f8099cf801f
Michael Byrne <michael.byrne@fcc.gov>
2013-11-05 09:06:33 -0500

    update w/ time ddl in tests; and mytime fields in all tables (plus index's)

--- data/create_tables.sql
+++ data/create_tables.sql
@@ -5,9 +5,10 @@
 CREATE TABLE mmba.tests
 (
   file_name character varying(75),
+  app_version character varying(10),
   submission_type character varying(15),
   unix_timezone numeric,
-  test_date character varying(35),
+  test_date timestamp without time zone,
   unix_time numeric,
   downspeed numeric,
   upspeed numeric,
@@ -34,7 +35,6 @@ ALTER TABLE mmba.tests
   OWNER TO postgres;
 
 
-
 -- Table: mmba."all"
 
 -- DROP TABLE mmba."all";
@@ -54,6 +54,7 @@ CREATE TABLE mmba."all"
   lp_count numeric,
   lp_average numeric,
   mytype character varying(200),
+  mytime character(17),
   gid integer
 )
 WITH (
@@ -71,6 +72,15 @@ CREATE INDEX mmba_all_gid_btree
   USING btree
   (gid );
 
+-- Index: mmba.mmba_all_mytime_btree
+
+-- DROP INDEX mmba.mmba_all_mytime_btree;
+
+CREATE INDEX mmba_all_mytime_btree
+  ON mmba."all"
+  USING btree
+  (mytime COLLATE pg_catalog."default" );
+
 -- Index: mmba.mmba_all_mytype_btree
 
 -- DROP INDEX mmba.mmba_all_mytype_btree;
@@ -79,6 +89,7 @@ CREATE INDEX mmba_all_mytype_btree
   ON mmba."all"
   USING btree
   (mytype COLLATE pg_catalog."default" );
+
 
 
 -- Table: mmba.provider
@@ -100,6 +111,7 @@ CREATE TABLE mmba.provider
   lp_count numeric,
   lp_average numeric,
   mytype character varying(200),
+  mytime character(17),
   gid integer
 )
 WITH (
@@ -117,6 +129,15 @@ CREATE INDEX mmba_provider_gid_btree
   USING btree
   (gid );
 
+-- Index: mmba.mmba_provider_mytime_btree
+
+-- DROP INDEX mmba.mmba_provider_mytime_btree;
+
+CREATE INDEX mmba_provider_mytime_btree
+  ON mmba.provider
+  USING btree
+  (mytime COLLATE pg_catalog."default" );
+
 -- Index: mmba.mmba_provider_mytype_btree
 
 -- DROP INDEX mmba.mmba_provider_mytype_btree;
@@ -126,51 +147,6 @@ CREATE INDEX mmba_provider_mytype_btree
   USING btree
   (mytype COLLATE pg_catalog."default" );
 
-
--- Table: mmba.network_type
-
--- DROP TABLE mmba.network_type;
-
-CREATE TABLE mmba.network_type
-(
-  ds_sum numeric,
-  ds_count numeric,
-  ds_average numeric,
-  us_sum numeric,
-  us_count numeric,
-  us_average numeric,
-  rtt_sum numeric,
-  rtt_count numeric,
-  rtt_average numeric,
-  lp_sum numeric,
-  lp_count numeric,
-  lp_average numeric,
-  mytype character varying(200),
-  gid integer
-)
-WITH (
-  OIDS=TRUE
-);
-ALTER TABLE mmba.network_type
-  OWNER TO postgres;
-
--- Index: mmba.mmba_network_type_gid_btree
-
--- DROP INDEX mmba.mmba_network_type_gid_btree;
-
-CREATE INDEX mmba_network_type_gid_btree
-  ON mmba.network_type
-  USING btree
-  (gid );
-
--- Index: mmba.mmba_network_type_mytype_btree
-
--- DROP INDEX mmba.mmba_network_type_mytype_btree;
-
-CREATE INDEX mmba_network_type_mytype_btree
-  ON mmba.network_type
-  USING btree
-  (mytype COLLATE pg_catalog."default" );
 
 
 -- Table: mmba.active_network_type
@@ -192,6 +168,7 @@ CREATE TABLE mmba.active_network_type
   lp_count numeric,
   lp_average numeric,
   mytype character varying(200),
+  mytime character(17),
   gid integer
 )
 WITH (
@@ -209,6 +186,15 @@ CREATE INDEX mmba_active_network_type_gid_btree
   USING btree
   (gid );
 
+-- Index: mmba.mmba_active_network_type_mytime_btree
+
+-- DROP INDEX mmba.mmba_active_network_type_mytime_btree;
+
+CREATE INDEX mmba_active_network_type_mytime_btree
+  ON mmba.active_network_type
+  USING btree
+  (mytime COLLATE pg_catalog."default" );
+
 -- Index: mmba.mmba_active_network_type_mytype_btree
 
 -- DROP INDEX mmba.mmba_active_network_type_mytype_btree;
@@ -217,6 +203,64 @@ CREATE INDEX mmba_active_network_type_mytype_btree
   ON mmba.active_network_type
   USING btree
   (mytype COLLATE pg_catalog."default" );
+
+
+
+-- Table: mmba.network_type
+
+-- DROP TABLE mmba.network_type;
+
+CREATE TABLE mmba.network_type
+(
+  ds_sum numeric,
+  ds_count numeric,
+  ds_average numeric,
+  us_sum numeric,
+  us_count numeric,
+  us_average numeric,
+  rtt_sum numeric,
+  rtt_count numeric,
+  rtt_average numeric,
+  lp_sum numeric,
+  lp_count numeric,
+  lp_average numeric,
+  mytype character varying(200),
+  mytime character(17),
+  gid integer
+)
+WITH (
+  OIDS=TRUE
+);
+ALTER TABLE mmba.network_type
+  OWNER TO postgres;
+
+-- Index: mmba.mmba_network_type_gid_btree
+
+-- DROP INDEX mmba.mmba_network_type_gid_btree;
+
+CREATE INDEX mmba_network_type_gid_btree
+  ON mmba.network_type
+  USING btree
+  (gid );
+
+-- Index: mmba.mmba_network_type_mytime_btree
+
+-- DROP INDEX mmba.mmba_network_type_mytime_btree;
+
+CREATE INDEX mmba_network_type_mytime_btree
+  ON mmba.network_type
+  USING btree
+  (mytime COLLATE pg_catalog."default" );
+
+-- Index: mmba.mmba_network_type_mytype_btree
+
+-- DROP INDEX mmba.mmba_network_type_mytype_btree;
+
+CREATE INDEX mmba_network_type_mytype_btree
+  ON mmba.network_type
+  USING btree
+  (mytype COLLATE pg_catalog."default" );
+
 
 
 -- Table: mmba.provider_active_network_type
@@ -238,6 +282,7 @@ CREATE TABLE mmba.provider_active_network_type
   lp_count numeric,
   lp_average numeric,
   mytype character varying(200),
+  mytime character(17),
   gid integer
 )
 WITH (
@@ -254,6 +299,15 @@ CREATE INDEX mmba_provider_active_network_type_gid_btree
   ON mmba.provider_active_network_type
   USING btree
   (gid );
+
+-- Index: mmba.mmba_provider_active_network_type_mytime_btree
+
+-- DROP INDEX mmba.mmba_provider_active_network_type_mytime_btree;
+
+CREATE INDEX mmba_provider_active_network_type_mytime_btree
+  ON mmba.provider_active_network_type
+  USING btree
+  (mytime COLLATE pg_catalog."default" );
 
 -- Index: mmba.mmba_provider_active_network_type_mytype_btree
 
@@ -284,6 +338,7 @@ CREATE TABLE mmba.provider_network_type
   lp_count numeric,
   lp_average numeric,
   mytype character varying(200),
+  mytime character(17),
   gid integer
 )
 WITH (
@@ -301,6 +356,15 @@ CREATE INDEX mmba_provider_network_type_gid_btree
   USING btree
   (gid );
 
+-- Index: mmba.mmba_provider_network_type_mytime_btree
+
+-- DROP INDEX mmba.mmba_provider_network_type_mytime_btree;
+
+CREATE INDEX mmba_provider_network_type_mytime_btree
+  ON mmba.provider_network_type
+  USING btree
+  (mytime COLLATE pg_catalog."default" );
+
 -- Index: mmba.mmba_provider_network_type_mytype_btree
 
 -- DROP INDEX mmba.mmba_provider_network_type_mytype_btree;
@@ -309,52 +373,4 @@ CREATE INDEX mmba_provider_network_type_mytype_btree
   ON mmba.provider_network_type
   USING btree
   (mytype COLLATE pg_catalog."default" );
-
-
--- Table: mmba.hex_75000
-
--- DROP TABLE mmba.hex_75000;
-
-CREATE TABLE mmba.hex_75000
-(
-  gid serial NOT NULL,
-  auto numeric,
-  y numeric,
-  x numeric,
-  auto_2 numeric,
-  region10 character varying(2),
-  division10 character varying(2),
-  statefp10 character varying(2),
-  statens10 character varying(8),
-  geoid10 character varying(2),
-  stusps10 character varying(2),
-  name10 character varying(100),
-  lsad10 character varying(2),
-  mtfcc10 character varying(5),
-  funcstat10 character varying(1),
-  aland10 numeric,
-  awater10 numeric,
-  intptlat10 character varying(11),
-  intptlon10 character varying(12),
-  shape_area numeric,
-  shape_len numeric,
-  geom geometry(MultiPolygon,900913),
-  CONSTRAINT hex_75000_pkey PRIMARY KEY (gid )
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE mmba.hex_75000
-  OWNER TO feomike;
-
--- Index: mmba.hex_75000_geom_gist
-
--- DROP INDEX mmba.hex_75000_geom_gist;
-
-CREATE INDEX hex_75000_geom_gist
-  ON mmba.hex_75000
-  USING gist
-  (geom );
-
-
 
